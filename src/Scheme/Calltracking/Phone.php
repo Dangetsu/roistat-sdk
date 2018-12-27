6d5f9b0e63f3b7e9905295d10efc062f9c7f166a
Vladislav Alatorcev(Dangetsu) <prototype9696@mail.ru>
2018-12-27 23:53:57 +0300

    method for buy new phones

--- src/Scheme/Calltracking/Phone.php
+++ src/Scheme/Calltracking/Phone.php
@@ -24,4 +24,16 @@ class Phone extends Scheme\AbstractScheme {
         $items = $this->_loadItems('project/calltracking/phone/list', $query, 'data');
         return $this->_buildEntity($items);
     }
+
+    /**
+     * @param string $prefix
+     * @param int $count
+     * @return Calltracking\Phone[]
+     * @throws Exception\AuthException
+     * @throws Exception\BasicException
+     */
+    public function buy($prefix, $count) {
+        $response = $this->_base->api()->send('project/calltracking/phone/buy', ['prefix' => $prefix, 'count' => $count], Engine\Api::METHOD_POST);
+        return $this->_buildEntity($response['data']);
+    }
 }
--- test/Calltracking/PhoneTest.php
+++ test/Calltracking/PhoneTest.php
@@ -29,4 +29,25 @@ class PhoneTest extends \Test\AbstractTest {
         $this->assertSame(0, $phone->getCallCount());
         $this->assertNotNull($phone->getScript());
     }
+
+    /**
+     * @throws Exception\AuthException
+     * @throws Exception\BasicException
+     */
+    public function testBuy() {
+        $this->_roistat->api()->addMockHandler($this->_createMockResponse($this->_getSavedResponse('Calltracking/PhoneBuy')));
+        $phones = (new Scheme\Calltracking\Phone($this->_roistat))->buy('499', 1);
+        $this->assertSame(1, count($phones));
+
+        $phone = $phones[0];
+        $this->assertSame(5, $phone->getId());
+        $this->assertSame('74991234567', $phone->getPhone());
+        $this->assertSame('7499', $phone->getPrefix());
+        $this->assertSame(0, $phone->getIsExternal());
+        $this->assertSame('2016-10-12T08:19:23+0000', $phone->getCreationDate());
+        $this->assertSame(0, $phone->getCallCount());
+        $this->assertNull($phone->getScriptId());
+        $this->assertNull($phone->getLastUseDate());
+        $this->assertNull($phone->getScript());
+    }
 }
